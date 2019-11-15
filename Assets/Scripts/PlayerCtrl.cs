8f6be90c917cf1c697a398930b576c23c71a7bb5
Cj-bc <cj.bc-sd@outlook.jp>
2019-11-15 19:07:25 +0900

    Add: Scripts/PlayerCtrl> add normal moving with AWSD

---
+++ Assets/Scripts/PlayerCtrl.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCtrl : MonoBehaviour
+{
+    public float speed = 20.0f;
+    public float jumpSpeed = 8.0f;
+    public float gravity = 7.8f;
+
+    private CharacterController controller;
+    private Vector3 velocity, forward, right;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (controller.isGrounded) {
+            forward = transform.transform.forward * Input.GetAxis("Vertical");
+            right = transform.transform.right * Input.GetAxis("Horizontal");
+            velocity = transform.TransformVector(forward + right) * speed;
+            if (Input.GetButton("Jump"))
+                velocity.y = jumpSpeed;
+        }
+        velocity.y -= gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
