e18bc4146623da977df625c6afb62cbd411de0a6
Cj-bc <cj.bc-sd@outlook.jp>
2019-11-15 23:30:32 +0900

    Change: Scripts/PlayerCtrl> allow user to move XZ axis a bit while jumping

--- Assets/Scripts/PlayerCtrl.cs
+++ Assets/Scripts/PlayerCtrl.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class PlayerCtrl : MonoBehaviour
 {
-    public float speed = 20.0f;
+    public float baseSpeed = 20.0f;
     public float jumpSpeed = 20.0f;
     public float gravity = 20.0f;
+    public float onAirMovementSpeed = 10.0f;
 
     private CharacterController controller;
     private Vector3 velocity, forward, right;
+    private float prev_up = 0.0f;
+    private float speed;
 
     // Start is called before the first frame update
     void Start()
@@ -20,13 +23,20 @@ public class PlayerCtrl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (controller.isGrounded) {
-            forward = transform.transform.forward * Input.GetAxis("Vertical");
-            right = transform.transform.right * Input.GetAxis("Horizontal");
-            velocity = transform.TransformVector(forward + right) * speed;
-            if (Input.GetButton("Jump"))
-                velocity.y = jumpSpeed;
-        }
+        if (controller.isGrounded)
+            speed = baseSpeed;
+        else
+            speed = onAirMovementSpeed;
+
+        forward = transform.transform.forward * Input.GetAxis("Vertical");  // W S
+        right = transform.transform.right * Input.GetAxis("Horizontal");  // A D
+        prev_up = velocity.y;
+        velocity = transform.TransformVector(forward + right) * speed;
+        velocity.y = prev_up;
+
+        if (controller.isGrounded&&Input.GetButton("Jump"))
+            velocity.y = jumpSpeed;
+
         velocity.y -= gravity * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
     }
