9619cd02804f7ef2eff6d24467891bed2f2ef404
Cj-bc <cj.bc-sd@outlook.jp>
2019-11-17 00:10:35 +0900

    Change: Scripts/Config> rename defaultConfig to KeyConfig
    Because:
      - it's smarter to separate configs into several properties

--- Assets/Scripts/Config.cs
+++ Assets/Scripts/Config.cs
@@ -13,13 +13,13 @@ public class Config : MonoBehaviour
         , ADS
         , Reload
     }
-    public Dictionary<UsedKeysList, KeyCode> defaultConfig;
+    public Dictionary<UsedKeysList, KeyCode> keyConfig;
 
     // I want to change this phase to load config from somewhere, like yaml
     void Awake() {
-        defaultConfig.Add(UsedKeysList.Crouch, KeyCode.LeftShift );
-        defaultConfig.Add(UsedKeysList.Jump  , KeyCode.Space );
-        defaultConfig.Add(UsedKeysList.Fire  , KeyCode.Mouse1 );
-        defaultConfig.Add(UsedKeysList.Reload, KeyCode.R );
+        keyConfig.Add(UsedKeysList.Crouch, KeyCode.LeftShift );
+        keyConfig.Add(UsedKeysList.Jump  , KeyCode.Space );
+        keyConfig.Add(UsedKeysList.Fire  , KeyCode.Mouse1 );
+        keyConfig.Add(UsedKeysList.Reload, KeyCode.R );
     }
 }
