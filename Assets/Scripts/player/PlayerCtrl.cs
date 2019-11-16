d21058dd0b40a71045f3c335acc01e27123f6681
Cj-bc <cj.bc-sd@outlook.jp>
2019-11-16 22:36:33 +0900

    Change: Scrpits/player/PlayerCtrl> change `speed`, used to calculate velocity, to 'PlayerStatus.speed'

--- Assets/Scripts/player/PlayerCtrl.cs
+++ Assets/Scripts/player/PlayerCtrl.cs
@@ -10,28 +10,29 @@ public class PlayerCtrl : MonoBehaviour
     public float onAirMovementSpeed = 10.0f;
 
     private CharacterController controller;
+    private PlayerStatus status;
     private Vector3 velocity, forward, right;
     private float prev_up = 0.0f;
-    private float speed;
 
     // Start is called before the first frame update
     void Start()
     {
         controller = GetComponent<CharacterController>();
+        status = GetComponent<PlayerStatus>();
     }
 
     // Update is called once per frame
     void Update()
     {
         if (controller.isGrounded)
-            speed = baseSpeed;
+            status.speed = baseSpeed;
         else
-            speed = onAirMovementSpeed;
+            status.speed = onAirMovementSpeed;
 
         forward = transform.transform.forward * Input.GetAxis("Vertical");  // W S
         right = transform.transform.right * Input.GetAxis("Horizontal");  // A D
         prev_up = velocity.y;
-        velocity = transform.TransformVector(forward + right) * speed;
+        velocity = transform.TransformVector(forward + right) * status.speed;
         velocity.y = prev_up;
 
         if (controller.isGrounded&&Input.GetButton("Jump"))
