3867809c1504431ba9a93981ed6b074b80d38733
Cj-bc <cj.bc-sd@outlook.jp>
2019-11-17 14:30:01 +0900

    Change: Scripts/Config> add cameraConfig setting to Config

--- Assets/Scripts/Config.cs
+++ Assets/Scripts/Config.cs
@@ -16,15 +16,21 @@ public class Config : MonoBehaviour
     }
 
     public Dictionary<UsedKeysList, KeyCode> keyConfig = new Dictionary<UsedKeysList, KeyCode>();
-
+    public CameraConfig cameraConfig;
     // I want to change this phase to load config from somewhere, like yaml
     void Awake() {
         keyConfig.Add(UsedKeysList.Crouch, KeyCode.LeftShift );
         keyConfig.Add(UsedKeysList.Jump  , KeyCode.Space );
         keyConfig.Add(UsedKeysList.Fire  , KeyCode.Mouse1 );
         keyConfig.Add(UsedKeysList.Reload, KeyCode.R );
+        cameraConfig = CameraConfig.defaultConfig();
     }
+
 }
+
+
+
+
 
 
 public class CameraConfig {
