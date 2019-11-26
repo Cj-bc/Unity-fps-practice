688a635aba98fa96022964bf3808142607f27664
Cj-bc <cj.bc-sd@outlook.jp>
2019-11-26 21:52:18 +0900

    Change: Scripts/WeaponCtrl> spawn bullet when PlayerStatus.isFiring is true

--- Assets/Scripts/WeaponCtrl.cs
+++ Assets/Scripts/WeaponCtrl.cs
@@ -11,12 +11,15 @@ public class WeaponCtrl : MonoBehaviour
     public int ammos = MAX_Ammo;
     public GunType guntype;
     public Vector3 offset;
+    public GameObject bullet;
 
     // Things to wanna add in the future
     // public Attachment scope;
     // public float recoil;
 
     private PlayerStatus status;
+    private float fireIntervalRemain;
+    private bool fired;
 
     // Start is called before the first frame update
     void Start()
@@ -28,7 +31,34 @@ public class WeaponCtrl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (status.isFiring)
+          fire(guntype, ammos);
+        else
+          fireIntervalRemain = 0;
     }
 
+    void fire(GunType gt, int ammo) {
+        if (ammo <= 0)
+            // TODO: play sound if run out of ammo
+            return;
 
+        switch(gt) {
+            case GunType.Semi:
+                if(fired) return;
+
+                Instantiate(bullet, transform.position, Quaternion.identity);
+                fired = true;
+                break;
+            case GunType.Auto:
+                if(0 <= fireIntervalRemain) {
+                    fireIntervalRemain -= Time.deltaTime;
+                    return;
+                }
+
+                Instantiate(bullet, transform.position, Quaternion.identity);
+
+                fireIntervalRemain = fireInterbal;
+                break;
+        }
+    }
 }
